221144761d84710ac85e218b3e65819908c0ebe3
SebasDC <SebasDC@users.noreply.github.com>
2020-07-30 11:13:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ var models = [
   {
     url: "./assets/articuno/scene.gltf",
     scale: "0.2 0.2 0.2",
-    rotation: "0 180 0",
+    rotation: "180 180 0",
     info: "Articuno, Lv. 80, HP 100/100",
   },
   {
